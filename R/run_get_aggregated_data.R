ff1b9bab5499903bbf14146b85b09eb5e9be8afa
fgrandesso <francesco.GRANDESSO@epicentre.msf.org>
2020-07-06 10:55:20 +0100

    Update run_get_aggregated_data.R
    To let Paut to correct the script

--- R/run_get_aggregated_data.R
+++ R/run_get_aggregated_data.R
@@ -7,9 +7,9 @@ path.sharepoint.agg.data <- file.path(path.sharepoint, "coordination", "Surveill
 
 agg_data_names <- c("sheet", "oc", "country", "project", "date", "week", "suspected", "probable", "confirmed", "non_cases", "unknown")
 
-df_weekly_aggregated <- excel_sheets(path.sharepoint.agg.data) %>% 
+dta_weekly_aggregated <- excel_sheets(path.sharepoint.agg.data) %>% 
   map_df(~{
-    oc <- read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "B1", col_names = FALSE) %>% pull()
+    oc      <- read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "B1", col_names = FALSE) %>% pull()
     country <- read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "D1", col_names = FALSE) %>% pull()
     project <- read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "F1", col_names = FALSE) %>% pull()
     
@@ -19,3 +19,20 @@ df_weekly_aggregated <- excel_sheets(path.sharepoint.agg.data) %>%
   select(sheet, oc, country, project, 1:7) %>% 
   set_names(agg_data_names)
 
+
+# NOT WORKING YET
+#dta_weekly_aggregated_dates <- excel_sheets(path.sharepoint.agg.data) %>% 
+#  map_df(~{
+#    date_first <- if (is_empty(read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "I1", col_names = FALSE))) {
+#      NA
+#      } else {
+#        read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "I1", col_names = FALSE) %>% pull()
+#      }
+#    
+#    date_last  <- if (is_empty(read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "K4", col_names = FALSE))) {
+#      NA
+#      } else {
+#        read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "K4", col_names = FALSE) %>% pull()
+#      }
+#    sheet <- excel_sheets(path.sharepoint.agg.data)
+#  })
