1d496502b3a79956c83bbd73df35b1f4379418dc
fgrandesso <francesco.GRANDESSO@epicentre.msf.org>
2020-07-09 11:31:23 +0100

    Update run_get_aggregated_data.R
    Modified target cells to find date_last location

--- R/run_get_aggregated_data.R
+++ R/run_get_aggregated_data.R
@@ -3,7 +3,7 @@ library(readxl)
 
 
 # Note: 'path.sharepoint' is set in setup.R
-path.sharepoint.agg.data <- file.path(path.sharepoint, "coordination", "Surveillance focal points coordination", "Aggregated reporting", "Report_covid_aggregate_all.xlsx")
+path.sharepoint.agg.data <- file.path(path.sharepoint, "coordination", "Surveillance focal points coordination", "Aggregated reporting", "Report_covid_aggregate_all_v2.xlsx")
 
 # Get weekly aggregated data
 agg_data_names <- c("sheet", "oc", "country", "project", "date", "week", "suspected", "probable", "confirmed", "non_cases", "unknown")
@@ -32,10 +32,10 @@ dta_project_aggregated_dates <- excel_sheets(path.sharepoint.agg.data) %>%
      date_first <- lubridate::as_date(date_first)
    }
    
-   if (is_empty(read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "K4", col_names = FALSE))) {
+   if (is_empty(read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "K1", col_names = FALSE))) {
      date_last  <- NA
    } else {
-     date_last <- read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "K4", col_names = FALSE) %>% pull()
+     date_last <- read_excel(path = path.sharepoint.agg.data, sheet = .x, range = "K1", col_names = FALSE) %>% pull()
      date_last <- lubridate::as_date(date_last)
    }
    
