c97aac532f3737b8435d740b4c15766a7281fe50
Francesco Grandesso <francesco.GRANDESSO@epicentre.msf.org>
2020-12-01 15:49:51 +0000

    EpiSitrep week 47-2020 final
    Fixed some bug. Update packages.

--- R/docx_section1_worldwide_analyses.R
+++ R/docx_section1_worldwide_analyses.R
@@ -74,7 +74,7 @@ my_doc <- add_end_section_2columns()
 ## - Map Cases count and trend
 my_doc <- add_figure_map_world_grid(
   object_name  = paste0('map_world_case_count_trend_grid', '_', week_report, '.png'),
-  figure_title = glue("Number of Covid-19 cases and cases trends estimated during the period from {format(date_max_report - period_trend - 1, '%d %B %Y')} to {format(date_max_report, '%d %B %Y')} ({period_trend} days)"))
+  figure_title = glue("Number of Covid-19 cases and cases trends estimated during the period from {format(date_max_report - (period_trend - 1), '%d %B %Y')} to {format(date_max_report, '%d %B %Y')} ({period_trend} days)"))
 
 my_doc <- add_end_section_continuous()
 
@@ -133,7 +133,7 @@ my_doc <- add_end_section_2columns()
 ## - Map
 my_doc <- add_figure_map_world_grid(
   object_name  = glue('map_world_death_count_trend_grid_{week_report}.png'), 
-  figure_title = glue('Mapping of number of Covid-19 associated deaths and deaths trends estimated during the period from {format(date_max_report - 11, "%d %B %Y")} to {format(date_max_report, "%d %B %Y")} (12 days)'))
+  figure_title = glue('Mapping of number of Covid-19 associated deaths and deaths trends estimated during the period from {format(date_max_report - (period_trend - 1), "%d %B %Y")} to {format(date_max_report, "%d %B %Y")} (12 days)'))
 
 
 
--- R/utils_get_data.R
+++ R/utils_get_data.R
@@ -366,27 +366,27 @@ get_iso_for_tests <- function() {
 
 
 
-#' Import MSF linelist dataset
-#' 
-#' @import
-get_msf_linelist <- function(path_local = path.local.msf.data, file_name = 'dta_MSF_linelist.RDS', path_remote = path.sharepoint.data, force = FALSE) {
-  
-  dta_path_local  <- file.path(path_local, file_name) 
-  dta_path_remote <- max(fs::dir_ls(path_remote, regexp = "[.]rds$"))
-  
-  if (!file.exists(dta_path_local) || force) {
-    
-    dta <- readRDS(dta_path_remote)
-    saveRDS(dta, file = dta_path_local)
-    
-  } else {
-    
-    dta <- readRDS(dta_path_local)
-    
-  }
-  
-  return(dta)
-}
+##' Import MSF linelist dataset
+##' 
+##' @import
+#get_msf_linelist <- function(path_local = path.local.msf.data, file_name = 'dta_MSF_linelist.RDS', path_remote = path.sharepoint.data, force = FALSE) {
+#  
+#  dta_path_local  <- file.path(path_local, file_name) 
+#  dta_path_remote <- max(fs::dir_ls(path_remote, regexp = "[.]rds$"))
+#  
+#  if (!file.exists(dta_path_local) || force) {
+#    
+#    dta <- readRDS(dta_path_remote)
+#    saveRDS(dta, file = dta_path_local)
+#    
+#  } else {
+#    
+#    dta <- readRDS(dta_path_local)
+#    
+#  }
+#  
+#  return(dta)
+#}
 
 
 
