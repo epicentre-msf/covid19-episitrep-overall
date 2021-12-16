8a6dda55b3d5ff5386fdedb57eaabba2becc65bf
Mathilde Mousset <mathilde.mousset@epicentre.msf.org>
2021-12-16 12:28:04 +0100

    fix mail sending for BGD

--- R/run_analysis_bgd.R
+++ R/run_analysis_bgd.R
@@ -14,16 +14,17 @@ rmarkdown::render(
 
 # install.packages(c("blastula", "keyring"))
 
+library(blastula)
+library(keyring)
+
+
 # Run once to generate credentials (puis supprimer du script)
 # blastula::create_smtp_creds_key(
 #   id = "",          # Name the Credential (un nom qui a du sens)
 #   user = "",        # User E-mail Address
-#   provider = "office365",       # Provider
-#   use_ssl = TRUE                # Ensure SSL is used
+#   provider = "office365"              # Ensure SSL is used
 # )
 
-library(blastula)
-library(keyring)
 
 # Add yourself
 if(Sys.info()[['user']] == "M-MOUSSET") {
